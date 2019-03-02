e332a7e5a841695d0933e11da26b91a83083e983
SumitGaurav <SumitGaurav@Lenovo-PC>
2019-03-01 23:24:20 -0800

    big house completion

--- src/GraphicsUnit/BigHouse.java
+++ src/GraphicsUnit/BigHouse.java
@@ -28,13 +28,6 @@ public class BigHouse extends Canvas
         
         myframe.setVisible(true);               
     }
-        JFrame myframe = new JFrame("Title: Big House");
-        Canvas canvas = new BigHouse();       
-        
-        myframe.add(canvas);
-        myframe.pack();
-        
-        myframe.setVisible(true); 
         
    public BigHouse()  //constructor - sets up the class
    {
@@ -45,18 +38,40 @@ public class BigHouse extends Canvas
 
    public void paint( Graphics window )
    {
-      //call bigHouse
+      bigHouse(window);
    }
 
    public void bigHouse( Graphics window )
    {
       window.setColor(Color.BLUE);
 
-      window.drawString( "BIG HOUSE ", 50, 50 );
-
+      window.drawString( "BIG HOUSE ", 300, 30 );
+      
+      //green top
+      window.setColor(Color.GREEN);
+      window.fillRect( 100, 50, 600, 150 );
+      
+      //blue hut
       window.setColor(Color.BLUE);
-
       window.fillRect( 200, 200, 400, 400 );
+      
+      
+      window.setColor(Color.YELLOW);
+      window.fillRect( 260, 270, 80, 105 );
+      
+      window.fillRect( 445, 270, 80, 105);
+      
+      window.setColor(Color.WHITE);
+      window.fillRect( 350, 450, 100, 300 );
+      
+      window.setColor(Color.GREEN);
+      window.fillOval( 50, 450, 100, 500 );
+      
+      window.setColor(Color.GREEN);
+      window.fillOval( 650, 450, 100, 500 );
+      
+      
+      
 
    }
 }
