e9a6cfd05b7f1098fde035e8e5391caa2cb292f7
SumitGaurav <SumitGaurav@Lenovo-PC>
2019-03-02 00:04:15 -0800

    Robot completion

--- src/GraphicsUnit/Robot.java
+++ src/GraphicsUnit/Robot.java
@@ -43,9 +43,11 @@ class Robot extends Canvas
       window.drawString("Robot LAB ", 35, 35 );
 
       //call head method
+      head(window);
       
       //call other methods
-      
+      upperBody(window);
+      lowerBody(window);
    }
 
    public void head( Graphics window )
@@ -53,22 +55,49 @@ class Robot extends Canvas
       window.setColor(Color.YELLOW);
 
       window.fillOval(300, 100, 200, 100);
-
-
 		//add more code here
+      
+      //eyes
+      window.setColor(Color.GREEN);
+      window.fillOval(330, 130, 20, 20);
+      window.fillOval(450, 130, 20, 20);
+      
+      //nose
+      window.setColor(Color.BLACK);
+      window.fillOval(400, 150, 15, 20);
+      
+      //mouth
+      window.setColor(Color.RED);
+      window.drawArc(350, 175, 100, 10, -30, -200);
 				
    }
 
    public void upperBody( Graphics window )
    {
-
-		//add more code here
+       //torso
+       window.setColor(Color.BLUE);
+       window.fillRect(330, 225, 150, 100);
+       
+       //left - arm
+       window.setColor(Color.BLACK);
+       window.drawLine(280, 175, 330, 225);
+       
+       //right-arm
+       window.drawLine(480, 225, 530, 175);
+       
    }
 
    public void lowerBody( Graphics window )
    {
-
-		//add more code here
-
+       //abdomin
+       window.setColor(Color.GRAY);
+       window.fillRect(330, 350, 150, 100);
+       
+       //left - leg
+       window.setColor(Color.BLACK);
+       window.drawLine(330, 450, 280, 550);
+       
+       //right-leg
+       window.drawLine(480, 450, 530, 550);
    }
 }
