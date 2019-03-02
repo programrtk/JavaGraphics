1ac8bc00434efab63135cae2bd219a5ca5aeb31a
SumitGaurav <SumitGaurav@Lenovo-PC>
2019-03-02 00:47:21 -0800

    commit

--- src/GraphicsUnit/Shape.java
+++ src/GraphicsUnit/Shape.java
@@ -11,6 +11,11 @@ package GraphicsUnit;
 import java.awt.Color;
 import java.awt.Graphics;
 
+import java.awt.Canvas;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
 public class Shape
 {
    //instance variables
@@ -22,8 +27,12 @@ public class Shape
 
    public Shape(int x, int y, int wid, int ht, Color col)
    {
-		xPos = x;
-		//finish this constructor
+	xPos = x;
+	//finish this constructor
+        yPos = y;
+        width = wid;
+        height = ht;
+        color = col;
    }
 
 
@@ -31,8 +40,10 @@ public class Shape
    {
       window.setColor(color);
       window.fillRect(xPos, yPos, width, height);
-
-      //draw whatever you want
+      window.drawOval(xPos, yPos, width, height);
+      window.drawLine( xPos + width/3 , yPos + height, xPos + width/3, yPos + height + height/10);
+      window.drawLine( xPos + 2*width/3 , yPos + height, xPos + 2*width/3, yPos + height + height/10);
+//draw whatever you want
       //    ^
       //  [ :: ]
       //    ()
@@ -47,3 +58,36 @@ public class Shape
    	return xPos+" "+yPos+" "+width+" "+height+" "+color;
    }
 }
+
+/*
+public class MyShape extends Canvas
+{
+
+    public static void main(String[] args) {
+              
+        // TODO code application logic here
+        
+        JFrame myframe = new JFrame("Title: MyShape");
+        Canvas canvas = new SmileyFace();       
+        
+        myframe.add(canvas);
+        myframe.pack();
+        
+        myframe.setVisible(true);               
+    }
+
+    public MyShape()    //constructor - sets up the class
+   {
+      setSize(800,600);
+      setBackground(Color.WHITE);
+      setVisible(true);
+   }
+
+   public void paint( Graphics window )
+   {
+      Shape shape_1 = new Shape(100, 100, 100, 100); 
+      shape_1.draw(window);
+   }
+
+}
+*/
