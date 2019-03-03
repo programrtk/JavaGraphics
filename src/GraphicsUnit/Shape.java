127158d6fab54e32c0e8d1a91379a408c3c6d0c9
programrtk <39711979+programrtk@users.noreply.github.com>
2019-03-02 17:58:27 -0800

    Update Shape.java

--- src/GraphicsUnit/Shape.java
+++ src/GraphicsUnit/Shape.java
@@ -59,35 +59,3 @@ public class Shape
    }
 }
 
-/*
-public class MyShape extends Canvas
-{
-
-    public static void main(String[] args) {
-              
-        // TODO code application logic here
-        
-        JFrame myframe = new JFrame("Title: MyShape");
-        Canvas canvas = new SmileyFace();       
-        
-        myframe.add(canvas);
-        myframe.pack();
-        
-        myframe.setVisible(true);               
-    }
-
-    public MyShape()    //constructor - sets up the class
-   {
-      setSize(800,600);
-      setBackground(Color.WHITE);
-      setVisible(true);
-   }
-
-   public void paint( Graphics window )
-   {
-      Shape shape_1 = new Shape(100, 100, 100, 100); 
-      shape_1.draw(window);
-   }
-
-}
-*/
